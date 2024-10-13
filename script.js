b9954f1666c798d0e7ba0e52a6280a1e14e755f4
Sartivian <fsartivian@gmail.com>
2024-10-13 14:13:00 +0700

    function new size and delete grid

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
+ 
+ 
  //reference to the container
  let container = document.getElementById("container");
+
+ let sizeButton = document.getElementById("size");
+
+ createGrid(16);
  
-function createOneDiv(){
+ 
+ let ukuran = 0;
+
+ sizeButton.addEventListener("click", ()=> {
+    ukuran = +prompt("Enter the size you want (max: 100)");
+    console.log(ukuran)
+    deleteAllGrid()
+    createGrid(ukuran)
+    listenMouse()
+ })
+
+function deleteAllGrid(){
+    while (allGrid.length !== 0) {
+        deleteGrid();
+    }
+}
+
+function deleteGrid(){
+    for (let i = 0; i < allGrid.length; i++){
+        allGrid[i].parentNode.removeChild(allGrid[i])
+    }
+}
+
+function createOneDiv(size){
     let oneDiv = document.createElement("div");
-    oneDiv.classList.add("grid")
+    let divSize = (962.22 / (size + 1)) - 2.22;
+    oneDiv.style.width = `${divSize}px`;
+    oneDiv.style.height = `${divSize}px`;
+    oneDiv.classList.add("grid");
     container.appendChild(oneDiv);
 }
 
-function createGrid() {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-         createOneDiv(); 
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+         createOneDiv(size); 
         }
     }
 }
 
-createGrid();
-//reference to grid box
-let allGrid = document.getElementsByClassName("grid");
 
-for (let i = 0; i < allGrid.length; i++) {
-    allGrid[i].addEventListener("mouseover", () =>{
-        allGrid[i].style.backgroundColor = "blue";
-    })
+//reference to grid box
+
+let allGrid = document.getElementsByClassName("grid");
+listenMouse();
+function listenMouse() {
+    for (let i = 0; i < allGrid.length; i++) {
+        allGrid[i].addEventListener("mouseover", () =>{
+            allGrid[i].style.backgroundColor = "blue";
+        })
+    }
 }
+
+
+
+
 
