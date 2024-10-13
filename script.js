ec3b9d6ace222a82cccdb550a2a8f0aaa3cc4b01
Sartivian <fsartivian@gmail.com>
2024-10-13 12:14:38 +0700

    Add funct to change grid color while hover

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
  //reference to the container
  let container = document.getElementById("container");
- let newLine = document.createElement("br");
  
 function createOneDiv(){
     let oneDiv = document.createElement("div");
+    oneDiv.classList.add("grid")
     container.appendChild(oneDiv);
 }
 
 function createGrid() {
-   
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
          createOneDiv(); 
         }
-        container.appendChild(newLine);
     }
 }
+
+createGrid();
+//reference to grid box
+let allGrid = document.getElementsByClassName("grid");
+
+for (let i = 0; i < allGrid.length; i++) {
+    allGrid[i].addEventListener("mouseover", () =>{
+        allGrid[i].style.backgroundColor = "blue";
+    })
+}
+
