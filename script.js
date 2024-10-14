0b998d1d17388dae8ce37d8ca9d85f1736b0e9cc
Sartivian <fsartivian@gmail.com>
2024-10-14 23:57:32 +0700

    Add rgb attemps hover

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
- 
+
  
  //reference to the container
  let container = document.getElementById("container");
 
  let sizeButton = document.getElementById("size");
+
+ let randomButton = document.getElementById("random")
+
+
 
  createGrid(16);
  
@@ -11,11 +15,14 @@
  let ukuran = 0;
 
  sizeButton.addEventListener("click", ()=> {
-    ukuran = +prompt("Enter the size you want (max: 100)");
+    do {
+        ukuran = +prompt("Enter the size you want (max: 100)");
+    }
+    while (ukuran > 100)
     console.log(ukuran)
     deleteAllGrid()
     createGrid(ukuran)
-    listenMouse()
+    randomColorHover()
  })
 
 function deleteAllGrid(){
@@ -32,7 +39,7 @@ function deleteGrid(){
 
 function createOneDiv(size){
     let oneDiv = document.createElement("div");
-    let divSize = (962.22 / (size + 1)) - 2.22;
+    let divSize = (961.82 / (size)) - 2.22;
     oneDiv.style.width = `${divSize}px`;
     oneDiv.style.height = `${divSize}px`;
     oneDiv.classList.add("grid");
@@ -51,13 +58,36 @@ function createGrid(size) {
 //reference to grid box
 
 let allGrid = document.getElementsByClassName("grid");
-listenMouse();
-function listenMouse() {
+
+function defaultColor() {
     for (let i = 0; i < allGrid.length; i++) {
         allGrid[i].addEventListener("mouseover", () =>{
             allGrid[i].style.backgroundColor = "blue";
+            allGrid[i].innerHTML = `${i + 1}`;
         })
     }
+}
+
+function randomColorHover() {
+    for (let i = 0; i < allGrid.length; i++) {
+        allGrid[i].addEventListener("mouseover", () =>{
+            changeRandom();
+            allGrid[i].innerHTML = `${i + 1}`;
+        })
+    }
+}
+randomColorHoverColor();
+
+let randNum = Math.floor(Math.random() * 255) + 1
+let randomColor = `rgb(${randNum},${randNum},${randNum})`
+
+function changeRandom(){
+    let randNumA = Math.floor(Math.random() * 255) + 1
+    let randNumB = Math.floor(Math.random() * 255) + 1
+    let randNumC = Math.floor(Math.random() * 255) + 1
+    let randomColor = `rgb(${randNumA},${randNumB},${randNumC})`
+    allGrid[0].style.backgroundColor = randomColor;
+    console.log(randomColor);
 }
 
 
